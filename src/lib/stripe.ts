97f1d52f13fcdf79092604a55cc4b1e40463ce9a
Ali <90032081+iamaliahad@users.noreply.github.com>
2022-11-13 04:51:40 +0800

    bug fixed

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,10 @@
 import Stripe from "stripe";
 
-const stripeServerSide = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2022-08-01",
-});
+const stripeServerSide = new Stripe(
+  process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!,
+  {
+    apiVersion: "2022-08-01",
+  }
+);
 
 export { stripeServerSide };
